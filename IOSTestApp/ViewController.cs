47a750f6a8c410a49a13de2820126b9bbfb05d66
Вася Пупкин <BomBimBom@bk.ru>
2018-12-06 17:03:10 +0300

    add

--- IOSTestApp/ViewController.cs
+++ IOSTestApp/ViewController.cs
@@ -1,4 +1,5 @@
 ﻿using Foundation;
+using IOSTestApp.localhost;
 using System;
 using UIKit;
 
@@ -6,19 +7,22 @@ namespace IOSTestApp
 {
     public partial class ViewController : UIViewController
     {
-        public ViewController (IntPtr handle) : base (handle)
+        public ViewController(IntPtr handle) : base(handle)
         {
         }
 
-        public override void ViewDidLoad ()
+        public override void ViewDidLoad()
         {
-            base.ViewDidLoad ();
+            base.ViewDidLoad();
+            var l_srv = new IOSTestApp.localhost.Service1();
+            bool val = true;
+            this.txtFIO.Text = l_srv.GetData(10, val);
             // Perform any additional setup after loading the view, typically from a nib.
         }
 
-        public override void DidReceiveMemoryWarning ()
+        public override void DidReceiveMemoryWarning()
         {
-            base.DidReceiveMemoryWarning ();
+            base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
         }
     }
--- IOSTestApp/ViewController.designer.cs
+++ IOSTestApp/ViewController.designer.cs
@@ -14,5 +14,25 @@ namespace IOSTestApp
     [Register ("ViewController")]
     partial class ViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView test { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtFIO { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (test != null) {
+                test.Dispose ();
+                test = null;
+            }
+
+            if (txtFIO != null) {
+                txtFIO.Dispose ();
+                txtFIO = null;
+            }
+        }
     }
 }
